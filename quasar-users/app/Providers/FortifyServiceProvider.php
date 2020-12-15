d15a7b72e0f37427f9f37b607b2c80a0097f2978
gmeben <gmeben@gmail.com>
2020-12-15 13:58:35 -0500

    feat: Add a view for the Fortify register route

--- quasar-users/app/Providers/FortifyServiceProvider.php
+++ quasar-users/app/Providers/FortifyServiceProvider.php
@@ -32,5 +32,9 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+
+        Fortify::registerView(function() {
+            return view('auth.register');
+        });
     }
 }
