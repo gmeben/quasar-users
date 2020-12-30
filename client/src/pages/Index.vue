57ab8946f14ab7ae8f8713dba299964ae44ee934
gmeben <gmeben@gmail.com>
2020-12-30 18:30:25 -0500

    ref: Remove Quasar boilerplate from index page

--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -6,8 +6,6 @@
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/CompositionComponent.vue';
 import { defineComponent, ref } from '@vue/composition-api';
 import axios from 'axios';
 
@@ -20,10 +18,9 @@ export default defineComponent({
   },
   methods: {
     loadData() {
-      this.$axios
+      axios
         .get('http://localhost:8080/api/hello')
         .then(res => {
-          console.log('res', res)
           this.message = res.data
         })
         .catch(error => {
@@ -34,34 +31,6 @@ export default defineComponent({
   mounted() {
     this.loadData()
   },
-  components: { ExampleComponent },
-  setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
-  }
+  setup() { }
 });
 </script>
