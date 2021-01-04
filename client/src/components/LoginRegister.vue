260adbbce26cf7bbe244f70b76b1e1ec050a3b26
gmeben <gmeben@gmail.com>
2021-01-04 09:48:07 -0500

    feat: Replace index page with authentication page

---
+++ client/src/components/LoginRegister.vue
@@ -0,0 +1,99 @@
+<template>
+  <q-form @submit="submitForm">
+    <q-input
+      v-if="tab == 'register'"
+      :value="loginUsername"
+      @input="setLoginUsername"
+      type="text"
+      label="Username"
+      input-style="font-size: 16px"
+      :disable="isLoading"
+      class="q-mb-md"
+      outlined
+    />
+    <q-input
+      v-if="tab == 'register'"
+      :value="loginName"
+      @input="setLoginName"
+      type="text"
+      label="Name (optional)"
+      input-style="font-size: 16px"
+      :disable="isLoading"
+      class="q-mb-md"
+      outlined
+    />
+    <q-input
+      :value="loginEmail"
+      @input="setLoginEmail"
+      type="email"
+      label="Email Address"
+      input-style="font-size: 16px"
+      :disable="isLoading"
+      class="q-mb-md"
+      outlined
+    />
+    <q-input
+      :value="loginPassword"
+      @input="setLoginPassword"
+      type="password"
+      label="Password"
+      input-style="font-size: 16px"
+      :disable="isLoading"
+      class="q-mb-md"
+      outlined
+    />
+    <q-banner
+      v-if="loginErrorStatus"
+      inline-actions
+      class="text-white bg-negative rounded-borders"
+      dense
+    >
+      <q-icon name="warning" /> {{ loginErrorMessage }}
+    </q-banner>
+    <q-btn
+      class="full-width q-mt-lg"
+      :label="tab"
+      type="submit"
+      color="primary"
+    ></q-btn>
+  </q-form>
+</template>
+
+<script>
+import { mapActions, mapGetters, mapMutations, mapState } from "vuex";
+export default {
+  props: ["tab"],
+  methods: {
+    ...mapActions("store", ["authenticateUser", "registerUser"]),
+    ...mapMutations("store", [
+      "setIsLoading",
+      "setLoginName",
+      "setLoginEmail",
+      "setLoginPassword",
+    ]),
+    submitForm() {
+      this.setIsLoading(true);
+
+      let name = this.loginName;
+      let email = this.loginEmail;
+      let password = this.loginPassword;
+
+      if (this.tab === "login") {
+        this.authenticateUser({ email, password });
+      } else {
+        this.registerUser({ name, email, password });
+      }
+    },
+  },
+  computed: {
+    ...mapGetters("store", [
+      "isLoading",
+      "loginName",
+      "loginEmail",
+      "loginPassword",
+      "loginErrorStatus",
+      "loginErrorMessage",
+    ]),
+  },
+};
+</script>
---
+++ client/src/pages/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-page class="bg-grey-3">
+    <div class="full-width q-pa-lg">
+      <div class="row justify-center">
+        <q-card>
+          <q-tabs
+            v-model="tab"
+            dense
+            class="text-grey"
+            active-color="primary"
+            indicator-color="primary"
+            align="justify"
+            narrow-indicator
+          >
+            <q-tab name="login" label="Login" />
+            <q-tab name="register" label="Register" />
+          </q-tabs>
+
+          <q-separator />
+
+          <q-tab-panels v-model="tab" animated class="q-pa-lg">
+            <q-tab-panel name="login">
+              <login-register :tab="tab" />
+            </q-tab-panel>
+
+            <q-tab-panel name="register">
+              <login-register :tab="tab" />
+            </q-tab-panel>
+          </q-tab-panels>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import LoginRegister from "../components/LoginRegister.vue";
+export default {
+  data() {
+    return {
+      tab: "login"
+    };
+  },
+  components: {
+    LoginRegister
+  }
+};
+</script>
