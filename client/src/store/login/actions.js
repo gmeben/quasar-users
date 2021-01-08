d96cee607acc20fa86e7b2c795b974d9b84abcb6
gmeben <gmeben@gmail.com>
2021-01-08 14:05:42 -0500

    feat: Add register and authenticate actions

--- client/src/store/login/actions.js
+++ client/src/store/login/actions.js
@@ -1,4 +1,38 @@
+import axios from 'axios'
 /*
 export function someAction (context) {
 }
 */
+export function registerUser({ commit, dispatch }, payload) {
+  console.log('register')
+  commit("setLoginError", {status: false, message: ""})
+
+  axios.post('http://localhost:8080/register').then(response => {
+    console.log('login', response)
+  })
+  .catch(error => {
+    commit("setLoginError", {status: true, message: "An error occurred while authenticating."})
+  })
+  .finally(() => {
+    commit("setIsLoading", false);
+  });
+
+}
+export function authenticateUser({ commit, dispatch }, payload) {
+  console.log('authenticate')
+  commit("setLoginError", {status: false, message: ""})
+
+  axios
+    .get('http://localhost:8080/sanctum/csrf-cookie')
+    .then(response => {
+      axios.post('http://localhost:8080/login').then(response => {
+        console.log('register', response)
+      })
+      .catch(error => {
+        commit("setLoginError", {status: true, message: "An error occurred while registering."})
+      })
+      .finally(() => {
+        commit("setIsLoading", false);
+      })
+    })
+}
